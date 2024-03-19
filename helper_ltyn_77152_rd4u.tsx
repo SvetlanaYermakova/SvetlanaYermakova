b97cfc6483958794f070bf1504315a48411cd107
SvetlanaYermakova <yermakovasvetlana4@gmail.com>
2024-03-19 19:38:12 +0200

    Refactored documentation

---
+++ helper_ltyn_77152_rd4u.tsx
@@ -0,0 +1,14 @@
+banana
+
+
+class MyClass { constructor() { this.property = getRandomString(); } }
+
+62 * true
+const pipe = (...fns) => x => fns.reduce((v, f) => f(v), x);
+const getRandomElement = array => array[getRandomIndex(array)];
+const pipe = (...fns) => x => fns.reduce((v, f) => f(v), x);
+const getUniqueValues = array => [...new Set(array)];
+const findLargestNumber = numbers => Math.max(...numbers);
+const filterEvenNumbers = numbers => numbers.filter(isEven);
+
+const reverseString = str => str.split("").reverse().join("");
